d761bd96424ebbcd51d46c28bd063fcb3ea3c611
Kenneth Reitz <me@kennethreitz.com>
2012-12-29 02:34:17 -0500

    read configuration file

---
+++ src/main/java/ElasticSearch.java
@@ -0,0 +1,23 @@
+import org.elasticsearch.node.NodeBuilder;
+// import static org.elasticsearch.node.NodeBuilder.*;
+
+import org.elasticsearch.common.settings.ImmutableSettings;
+
+
+public class ElasticSearch {
+  public static void main(String[] args) {
+    NodeBuilder nb = NodeBuilder.nodeBuilder();
+
+    nb.local(true);
+    nb.settings(ImmutableSettings.settingsBuilder().loadFromClasspath("elasticsearch.yml"));
+    nb.node();
+
+    while (true) {
+      try {
+        Thread.currentThread().sleep(60000);
+      } catch (Exception e) {
+        /* ignore */
+      }
+    }
+  }
+}
