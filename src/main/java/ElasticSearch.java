34eddef2e83b1246c1e1960eefb3940d8472fd94
Nina Pavlich <nina@hello-upswell.com>
2017-08-03 13:49:59 -0700

    hello world

--- src/main/java/ElasticSearch.java
+++ src/main/java/ElasticSearch.java
@@ -6,18 +6,20 @@ import org.elasticsearch.common.settings.ImmutableSettings;
 
 public class ElasticSearch {
   public static void main(String[] args) {
-    NodeBuilder nb = NodeBuilder.nodeBuilder();
+    // NodeBuilder nb = NodeBuilder.nodeBuilder();
 
-    nb.local(true);
-    nb.settings(ImmutableSettings.settingsBuilder().loadFromClasspath("elasticsearch.yml"));
-    nb.node();
+    // nb.local(true);
+    // nb.settings(ImmutableSettings.settingsBuilder().loadFromClasspath("elasticsearch.yml"));
+    // nb.node();
 
-    while (true) {
-      try {
-        Thread.currentThread().sleep(60000);
-      } catch (Exception e) {
-        /* ignore */
-      }
-    }
+    // while (true) {
+    //   try {
+    //     Thread.currentThread().sleep(60000);
+    //   } catch (Exception e) {
+    //     /* ignore */
+    //   }
+    // }
+
+    System.out.println("Hello, World");
   }
 }
